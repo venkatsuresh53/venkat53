7755429a1b6b81cb13b6a168c65d434d4445f310
venkatsuresh53 <transmit2venkat@gmail.com>
2022-02-15 11:15:46 +0530

    message

---
+++ src/org/bank/AxisBank.java
@@ -0,0 +1,14 @@
+package org.bank;
+
+public class AxisBank extends BankInfo {
+public void deposit() {
+System.out.println("deposit 50000");
+		
+}
+public static void main(String[] args) {
+	AxisBank ab=new AxisBank();
+	ab.deposit();
+	ab.saving();
+	ab.fixed();
+}		
+}
---
+++ src/org/bank/BankInfo.java
@@ -0,0 +1,16 @@
+package org.bank;
+
+public class BankInfo {
+	public void saving() {
+		System.out.println("saving 5%");
+		
+	}
+	
+	public void fixed() {
+System.out.println("fixed 100000");
+	}
+	public void deposit() {
+		System.out.println("deposit 10000");
+	
+	}
+}
